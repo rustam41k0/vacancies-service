5be7789806c0f377ed7f34d5970f6e02085a4e7f
Rustam Sharipov <rustamskysan@gmail.com>
2024-05-15 23:21:02 +0500

    refactoring

--- src/main.py
+++ src/main.py
@@ -21,5 +21,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)  #, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8000)  #, reload=True)
